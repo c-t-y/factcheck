491ba9bd027d3dcb4ed286819b8cc00e49c615b8
Charlie <charlie.t.yin@gmail.com>
2023-04-13 08:13:19 -0700

    Update background.js

--- background.js
+++ background.js
@@ -13,6 +13,31 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === CONTEXT_MENU_ID) {
     const selectedText = info.selectionText;
     console.log(selectedText);
+
+    // const { Configuration, OpenAIApi } = require("openai");
+
+    // const configuration = new Configuration({
+    //   apiKey: "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",
+    // });
+
+    // const prompt = "what is the best color";
+
+
+    // async function getResponse(topic) {
+    //   const openai = new OpenAIApi(configuration);
+    //   const completion = await openai.createCompletion({
+    //     model: "text-davinci-003",
+    //     prompt: topic,
+    //     max_tokens: 1024,
+    //     n: 1,
+    //     stop: null,
+    //     temperature: 0.7
+    //   });
+    //   console.log(completion.data.choices[0].text);
+    // }
+    // getResponse(prompt);
+
+
   }
 });
 
